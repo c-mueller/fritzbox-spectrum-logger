3292bcb4c0e695353a891df15f6886924c5f152b
Christian Müller <cmueller.dev@gmail.com>
2018-01-19 00:50:23 +0100

    Implement Logging

--- repository/repository.go
+++ repository/repository.go
@@ -1,6 +1,70 @@
 package repository
 
+import (
+    "github.com/boltdb/bolt"
+    "github.com/c-mueller/fritzbox-spectrum-logger/fritz"
+    "time"
+    "fmt"
+)
+
+func NewRepository(path string) (*Repository, error) {
+    db, err := bolt.Open(path, 0777, &bolt.Options{})
+
+    if err != nil {
+        return nil, err
+    }
+
+    err = initDb(db)
+
+    return &Repository{
+        db: db,
+    }, nil
+}
+
+func (r *Repository) Insert(spectrum *fritz.Spectrum) error {
+    tx, err := r.db.Begin(true)
+    defer tx.Commit()
+    if err != nil {
+        return err
+    }
+
+    timestamp := time.Unix(spectrum.Timestamp, 0)
+    year := fmt.Sprintf("%d", timestamp.Year())
+    month := timestamp.Month().String()
+    day := fmt.Sprintf("%d", timestamp.Day())
+
+    spectraBucket, _ := tx.CreateBucketIfNotExists([]byte(SpectrumListBucketName))
+
+    yearBucket, _ := spectraBucket.CreateBucketIfNotExists([]byte(year))
+    monthBucket, _ := yearBucket.CreateBucketIfNotExists([]byte(month))
+    dayBucket, _ := monthBucket.CreateBucketIfNotExists([]byte(day))
+
+    jsonData, err := spectrum.JSON()
+    if err != nil {
+        return err
+    }
+    err = dayBucket.Put([]byte(string(fmt.Sprintf("%d", timestamp.Unix()))), jsonData)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
 
 func (r *Repository) Close() error {
     return r.db.Close()
+}
+
+func initDb(db *bolt.DB) error {
+    tx, err := db.Begin(true)
+    defer tx.Commit()
+
+    if err != nil {
+        return err
+    }
+    _, err = tx.CreateBucketIfNotExists([]byte(SpectrumListBucketName))
+    if err != nil {
+        return err
+    }
+    return nil
 }

d0a285241482a3bad8f5663654a6ac6594e9ed90
Christian Müller <cmueller.dev@gmail.com>
2018-02-03 02:17:16 +0100

    Some API implementations

--- application/application.go
+++ application/application.go
@@ -6,8 +6,6 @@ import (
     "github.com/gin-gonic/gin"
     "github.com/c-mueller/fritzbox-spectrum-logger/repository"
     "time"
-    "io/ioutil"
-    "encoding/json"
 )
 
 var log = logging.MustGetLogger("server")
@@ -21,10 +19,11 @@ func LaunchApplication(configPath string) *Application {
         return nil
     }
     return &Application{
-        config:    *cfg,
-        bindAdr:   cfg.BindAddress,
-        state:     IDLE,
-        startTime: time.Now(),
+        config:            *cfg,
+        bindAdr:           cfg.BindAddress,
+        state:             IDLE,
+        startTime:         time.Now(),
+        sessionLogCounter: 0,
     }
 }
 
@@ -53,37 +52,6 @@ func (a *Application) registerHTTPMappings(engine *gin.Engine) {
     engine.GET("/api/config", a.getConfiguration)
 
     engine.POST("/api/config", a.updateConfig)
-}
-
-func (a *Application) getStatus(ctx *gin.Context) {
-    ctx.JSON(200, StatusResponse{
-        State:  a.state.String(),
-        Uptime: int64(time.Since(a.startTime).Seconds()),
-    })
-}
-
-func (a *Application) getConfiguration(ctx *gin.Context) {
-    var cfg config.Configuration = a.config
-    cfg.Credentials.Password = "HIDDEN"
-    ctx.JSON(200, cfg)
-}
-
-func (a *Application) updateConfig(ctx *gin.Context) {
-    data, err := ioutil.ReadAll(ctx.Request.Body)
-    if err != nil {
-        ctx.AbortWithError(400, InvalidBodyError)
-        return
-    }
-    var parsedBody *config.Configuration
-    err = json.Unmarshal(data, &parsedBody)
-    if err != nil {
-        ctx.AbortWithError(400, JSONParsingError)
-        return
-    }
-    a.config.Update(parsedBody)
-    err = a.config.Write()
-    if err != nil {
-        ctx.AbortWithError(500, FileSystemError)
-    }
-    ctx.String(200, "")
+    engine.POST("/api/control/start", a.startCollecting)
+    engine.POST("/api/control/stop", a.stopCollecting)
 }
--- application/model.go
+++ application/model.go
@@ -8,15 +8,23 @@ import (
 )
 
 type Application struct {
-    config    config.Configuration
-    bindAdr   string
-    repo      *repository.Repository
-    session   *fritz.Session
-    state     ApplicationState
-    startTime time.Time
+    config            config.Configuration
+    bindAdr           string
+    repo              *repository.Repository
+    session           *fritz.Session
+    state             ApplicationState
+    startTime         time.Time
+    updateTicker      *time.Ticker
+    sessionLogCounter int64
 }
 
 type StatusResponse struct {
-    State  string `json:"state"`
-    Uptime int64  `json:"uptime"`
+    State         string `json:"state"`
+    Uptime        int64  `json:"uptime"`
+    SpectrumCount int64  `json:"spectrum_count"`
+}
+
+type InfoResponse struct {
+    State   string `json:"state"`
+    Message string `json:"message"`
 }

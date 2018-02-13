6d5b2852114773c9638d47cf38ee155be9de06a6
Christian Müller <cmueller.dev@gmail.com>
2018-02-13 21:34:14 +0100

    Update LICENSE to GPL-3.0

--- application/constants.go
+++ application/constants.go
@@ -1,20 +1,35 @@
+// Fritz!Box Spectrum Logger (https://github.com/c-mueller/fritzbox-spectrum-logger).
+// Copyright (c) 2018 Christian Müller<cmueller.dev@gmail.com>.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 package application
 
 import (
 	"errors"
 )
 
-type ApplicationState int
+type APIState int
 
-const IDLE ApplicationState = iota
-const LOGGING ApplicationState = IDLE + 1
-const ERROR ApplicationState = LOGGING + 1
+const IDLE APIState = iota
+const LOGGING APIState = IDLE + 1
+const ERROR APIState = LOGGING + 1
 
 var InvalidBodyError = errors.New("application: Could not deserialize request body. The body has to be JSON")
 var JSONParsingError = errors.New("application: Could not parse JSON")
 var FileSystemError = errors.New("application: Fileaccess has failed")
 
-func (s ApplicationState) String() string {
+func (s APIState) String() string {
 	if s == IDLE {
 		return "IDLE"
 	} else if s == LOGGING {

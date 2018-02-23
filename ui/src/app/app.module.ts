f0a36a81cd5e46e8ef4cfd79f5dc5a97414dc6a5
Christian Müller <cmueller.dev@gmail.com>
2018-02-23 04:26:42 +0100

    Add Base Components (Pages)

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,21 +1,66 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
+import {AppComponent} from './app.component';
+import {NgbAlertModule, NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule, Routes} from '@angular/router';
+import {NotFoundComponent} from './pages/not-found/not-found.component';
+import {StatusComponent} from './pages/status/status.component';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
+import {AboutComponent} from './pages/about/about.component';
+import {ConfigComponent} from './pages/config/config.component';
+import {SpectraComponent} from './pages/spectra/spectra.component';
 
-import { AppComponent } from './app.component';
-import {NgbAlertModule, NgbModule} from "@ng-bootstrap/ng-bootstrap";
-
+const appRoutes: Routes = [
+  {
+    path: 'status',
+    component: StatusComponent
+  },
+  {
+    path: 'spectra',
+    component: SpectraComponent
+  },
+  {
+    path: 'config',
+    component: ConfigComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: '',
+    redirectTo: '/status',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ConfigComponent,
+    SpectraComponent,
+    StatusComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     NgbModule.forRoot(),
     NgbAlertModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ ui/src/app/pages/spectra/spectra.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spectra',
+  templateUrl: './spectra.component.html',
+  styleUrls: ['./spectra.component.css']
+})
+export class SpectraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

00e5f8b340dae627c07b6b183198e28ad1b9d889
Christian Müller <cmueller.dev@gmail.com>
2018-03-15 01:54:01 +0100

    Implement Start/Stop in the UI
    - Remove Spec files
    - Attempt to fix Request URL in production
    - Add License Comments in TypeScript Files

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -1,9 +1,19 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
+// Fritz!Box Spectrum Logger (https://github.com/c-mueller/fritzbox-spectrum-logger).
+// Copyright (c) 2018 Christian Müller <cmueller.dev@gmail.com>.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 export const environment = {
   production: false,
-  host: 'http://127.0.0.1:8080'
+  host: 'http://127.0.0.1:8080/api'
 };
